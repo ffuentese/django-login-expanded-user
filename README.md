fe6fe0e3d7d84347323f553c924dbf9546382255
Francisco Fuentes <txi.francisco@gmail.com>
2017-02-24 21:34:46 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django Login with expanded profile
+
+Django by default includes a user model that's very useful but it's limited to a user and a password so it's better to add it a few attributes. [This tutorial](https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html) shows how to add them and I followed the instructions. Also to complement this project I also applied the authentication views shown in [another tutorial](https://simpleisbetterthancomplex.com/tutorial/2016/06/27/how-to-use-djangos-built-in-login-system.html) to use the django login system from outside the admin panel on my own.
+
+Later on, I'll add CSS to make it look pretty.
---
+++ core/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    STUDENT = 1
+    TEACHER = 2
+    SUPERVISOR = 3
+    ROLE_CHOICES = (
+        (STUDENT, 'Student'),
+        (TEACHER, 'Teacher'),
+        (SUPERVISOR, 'Supervisor'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+
+    def __unicode__(self):  # __unicode__ for Python 2
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
